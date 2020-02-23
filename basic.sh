c0cc52de20c47aed4886071e42628b800d15c49e
d3ni <deni1myftiu@gmail.com>
2020-02-23 16:01:09 +0100

    added nmap to basic.sh

--- basic.sh
+++ basic.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt install upgrade -y
 sudo apt install -y git python3.7 python3-pip python3.7-dev socat perl build-essential \
                  gcc cmake php7.2 openjdk-11-jdk openjdk-11-jdk-headless \
-                 openjdk-11-jre binwalk openssl vim
+                 openjdk-11-jre binwalk openssl vim nmap
 python3.7 -m pip install --upgrade pip
 python3.7 -m pip install virtualenv
 python3.7 -m virtualenv pwnenv
