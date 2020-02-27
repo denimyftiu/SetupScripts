8cbee93fd0c348bee8d22b9f9b819cd479c61e63
d3ni <deni1myftiu@gmail.com>
2020-02-27 10:31:49 +0100

    added nginx and sendmal and set the python env to path TODO must me aded to bash rc

--- basic.sh
+++ basic.sh
@@ -2,9 +2,11 @@
 sudo apt update && sudo apt install upgrade -y
 sudo apt install -y git python3.7 python3-pip python3.7-dev socat perl build-essential \
                  gcc cmake php7.2 openjdk-11-jdk openjdk-11-jdk-headless \
-                 openjdk-11-jre binwalk openssl vim nmap
+                 openjdk-11-jre binwalk openssl vim nmap nginx sendmail
+sudo apt autoremove && sudo apt clean
 python3.7 -m pip install --upgrade pip
 python3.7 -m pip install virtualenv
 python3.7 -m virtualenv pwnenv
+export PATH="/home/$USER/.local/bin:$PATH"
 source pwnenv/bin/activate
 pip3.7 install pwntools
