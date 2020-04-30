39ffd3bc015da7de9cd7b462ebbce89f91a1a585
D3ni Myftiu <deni2myftiu@gmail.com>
2020-04-30 15:09:17 +0200

    all vim stuff updated in a new dir

--- basic.sh
+++ basic.sh
@@ -4,6 +4,7 @@ sudo apt update && sudo apt install upgrade -y
 sudo apt install -y git python3.7 python3-pip python3.7-dev socat perl build-essential \
                  gcc cmake php7.2 openjdk-11-jdk openjdk-11-jdk-headless \
                  openjdk-11-jre binwalk openssl vim nmap nginx sendmail
+
 sudo apt autoremove && sudo apt clean
 python3.7 -m pip install --upgrade pip
 python3.7 -m pip install virtualenv
