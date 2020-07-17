471cdc96772c91e6ee1b0da604ea7c100e639409
D3ni Myftiu <deni2myftiu@gmail.com>
2020-07-17 16:14:26 +0200

    Dont need ugly php in my life no more.

--- basic.sh
+++ basic.sh
@@ -2,7 +2,8 @@
 
 sudo apt update && sudo apt install upgrade -y
 sudo apt install -y git python3.8 python3-pip python3.8-dev socat perl build-essential \
-                 gcc cmake php7.2 openjdk-11-jdk openjdk-11-jdk-headless \
+                 gcc cmake openjdk-11-jdk openjdk-11-jdk-headless \
                  openjdk-11-jre binwalk openssl vim nmap nginx sendmail
 
-sudo apt autoremove && sudo apt clean
+cat ./addtobashrc.txt >> ~/.bashrc
+sudo apt autoremove -y && sudo apt clean
