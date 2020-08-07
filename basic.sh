837335955ebe231188d58f5c8ceae70a8df2a4bf
D3ni Myftiu <deni2myftiu@gmail.com>
2020-08-07 12:51:43 +0200

    Fonts and change bashrc a little.

--- basic.sh
+++ basic.sh
@@ -3,7 +3,9 @@
 sudo apt update && sudo apt install upgrade -y
 sudo apt install -y git python3.8 python3-pip python3.8-dev socat perl build-essential \
                  gcc cmake openjdk-11-jdk openjdk-11-jdk-headless \
-                 openjdk-11-jre binwalk openssl vim nmap nginx sendmail
+                 openjdk-11-jre binwalk openssl vim nmap nginx sendmail netcat \
+                 net-tools rkhunter
 
+mkdir ~/environments
 cat ./addtobashrc.txt >> ~/.bashrc
 sudo apt autoremove -y && sudo apt clean
